c54ce35d9cd1881dae4a27461df88223df69ecc8
Xue.Yikang <xykmoyada@gmail.com>
2024-01-21 15:38:18 +0800

    Update socks5.sh

--- socks5.sh
+++ socks5.sh
@@ -1,17 +1,16 @@
 #!/usr/bin/env bash
 
-#wget -N --no-check-certificate https://raw.githubusercontent.com/moyada/res/master/socks5.sh
-
 run() {
   wget -O socks5 https://raw.githubusercontent.com/moyada/res/master/socks5
 
-  if [ ! -f "s5.pid" ];then
+  if [ -f "s5.pid" ];then
     echo "kill exist socks5"
     kill `cat s5.pid`
     rm -f s5.pid
   fi
 
   echo "open socks5"
+  chmod +x socks5
   nohup ./socks5 >> out.log 2>&1 &
   echo $! > s5.pid
 }
